17b031a7544eb33e91c1a3a623860f453788329f
Griez <835132735@qq.com>
2018-02-05 15:22:41 +0800

    version.2

---
+++ www/config_override.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @File  : config_override.py
+# @Author: lvconl
+# @Date  : 18-1-27
+#@Software : PyCharm
+
+configs = {
+    'db':{
+        'host':'127.0.0.1'
+    }
+}
